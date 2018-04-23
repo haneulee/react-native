ce522c02256da9b9709a6e4a3fd6e39bbf8c10fc
haneulee <lovesky4294@gmail.com>
2018-04-23 16:59:52 +0900

    reactive-native first commit

---
+++ react-native-app/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { AppRegistry, Image, StyleSheet, Text, View } from 'react-native';
+
+export default class Bananas extends React.Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}}/>
+    );
+  }
+}
+
+AppRegistry.registerComponent('App', () => Bananas);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
