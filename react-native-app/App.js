6d91b97fa906c67841e3420dc958b33e0f0a5a7d
haneulee <lovesky4294@gmail.com>
2018-05-03 15:04:33 +0900

    counter 추가

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import { AppRegistry, Image, StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, View } from 'react-native';
+import { Provider, connect } from 'react-redux';
+import store from './store/store.js';
+import CounterContainer from './containers/CounterContainer.js';
 
-export default class Bananas extends React.Component {
+export default class App extends Component {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
-      <Image source={pic} style={{width: 193, height: 110}}/>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <CounterContainer/>
+        </View>
+      </Provider>
     );
   }
 }
 
-AppRegistry.registerComponent('App', () => Bananas);
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
